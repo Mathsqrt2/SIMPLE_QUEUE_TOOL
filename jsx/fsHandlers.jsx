67f1d9693f3ddf948eb015972df50a854550bf9f
Mathsqrt2 <micho.bugajski@gmail.com>
2024-05-14 22:24:20 +0200

    checking first launch moved to localStoreage

--- jsx/fsHandlers.jsx
+++ jsx/fsHandlers.jsx
@@ -1,26 +1,3 @@
-function isItFirstUseJSX(path) {
-    var newResponse = {
-        isItFirstUse: true,
-        actionTime: null,
-    };
-
-    var logPath = $.runScript.fixPath(path) + $.runScript.fixPath("\\config\\firstLaunchLog.json");
-    var firstLaunchLog = new File(logPath);
-
-    if (firstLaunchLog.exists) {
-        newResponse.isItFirstUse = false;
-    } else {
-        var currentTime = new Date();
-        newResponse.actionTime = currentTime.getTime();
-        firstLaunchLog.open("w");
-        firstLaunchLog.write(JSON.stringify(newResponse));
-        firstLaunchLog.close();
-    }
-
-    var output = JSON.stringify(newResponse);
-    return output;
-}
-
 function setOSValue(csinfo) {
     var obj = JSON.parse(csinfo);
     currentOS = obj.index;
